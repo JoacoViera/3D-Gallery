ed10a55e913d910fa9a89d0b299f9d78ec57f071
Joaquin <joaquin.viera@outlook.com>
2019-09-05 17:00:54 -0300

    filtro en barra busqueda hecho

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
     // Listener de los enlaces para filtrar por cada cateogria y todos
     const enlaces = document.querySelectorAll('#categorias a');
     const imagenes = document.querySelectorAll('.contenedor_tarjeta');
+
     for (const enlace of enlaces) {
         enlace.addEventListener('click', (evento) => {
             //cambiar a negrita el tipo seleccionado
@@ -13,6 +14,7 @@ window.addEventListener('load', () => {
             evento.target.classList.add('activo');
 
             // traer la categoria seleccionda y marcar elementos
+            // se agregara el estilo display none a los de las categorias no seleccionadas y display block a los que querramos ver
             const categoria = evento.target.innerHTML.toLowerCase();
             for(const tarjeta of imagenes){
                 if(categoria === 'todos'){
@@ -28,5 +30,25 @@ window.addEventListener('load', () => {
             }
         });
     }
-
+    
+     //Listener para la barra de busqueda
+     //El ciclo for es exactamente el mismo
+     //Se podria incluso crear una funcion para mejorar la calidad del codigo y sea mas legible 
+     document.querySelector('#barra-busqueda').addEventListener('input',
+     (evento) => {
+         const busqueda = evento.target.value;
+         for(const tarjeta of imagenes){
+            if(busqueda === 'todos'){
+                tarjeta.style.display = 'block';
+            }else{
+                if(!tarjeta.className.includes(busqueda)){
+                    tarjeta.style.display = 'none';
+                }
+                else{
+                    tarjeta.style.display = 'block';
+                }
+            }
+        }
+     });
+ 
 });
